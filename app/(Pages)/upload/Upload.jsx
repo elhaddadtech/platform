ef6aed23281c3fa07b58557ff8ea0274494a7358
ELMUSTAPHA-MOUJANE <e.moujane@uca.ac.ma>
2024-02-19 14:03:55 +0100

    Juste pour le test

--- app/(Pages)/upload/Upload.jsx
+++ app/(Pages)/upload/Upload.jsx
@@ -1,13 +1,13 @@
 "use client"
 import CsvReader from "../../Components/CsvReader"
 import TableData from "../../Components/TableData"
-import LoginForm from "../../Components/LoginForm"
+
 function Upload() {
   return (
     <div className='container mx-auto  flex flex-col gap-10'>
        <div className='h-16 '> <CsvReader/></div>
       <div className='m' > <TableData /></div>
-      <LoginForm/>
+      
     </div>
   )
 }
